c6e42bc3f42a57c316b3d18ef5e966b5087ebed4
shishirtripathiBajaj <shishir.tripathi@bajajtechnologyservices.com>
2026-01-06 14:26:14 +0530

    no-logs

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -16,5 +16,4 @@ export default function decorate(block) {
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '250' }])));
   block.replaceChildren(ul);
-  console.log("this is my log");
 }
