658be155e9a5f87169f8a05a20578bc0dc729667
shishirtripathiBajaj <shishir.tripathi@bajajtechnologyservices.com>
2026-01-06 13:22:30 +0530

    attemptTwo

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -7,12 +7,14 @@ export default function decorate(block) {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+        div.addEventListener("click", function(){ alert("card img clicked"); } );
+      }
       else div.className = 'cards-card-body';
     });
     ul.append(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '250' }])));
-  ul.querySelectorAll('picture > img').forEach((img) => img.addEventListener("click", function(){ alert("card img clicked"); } ) );
   block.replaceChildren(ul);
 }
